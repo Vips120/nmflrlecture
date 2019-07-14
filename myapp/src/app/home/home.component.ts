5e6a7830121e84b5e63b9e0326debc6dc3d2e929
vips120 <vipulsingh0000@gmail.com>
2019-07-14 13:30:14 +0530

    #reactiveextensionvipul

--- myapp/src/app/home/home.component.ts
+++ myapp/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Renderer, ElementRef } from '@angular/core';
+import {fromEvent} from 'rxjs';
+import { map,filter,debounceTime } from "rxjs/operators";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private RD: Renderer, private ER: ElementRef) {
+    // let Search = this.ER.nativeElement.querySelector('#search');
+    // this.RD.setElementStyle(Search, 'BackgroundColor', 'red')
+  }
 
   ngOnInit() {
+  }
+  Search(){
+    let Search = this.ER.nativeElement.querySelector('#search');
+  let p = fromEvent(Search, 'keyup')
+                  .pipe(map((e:any) => e.target.value),
+                        filter(text => text.length>=3),
+                        debounceTime(2000)
+                  );
+  p.subscribe(data => console.log(data));
   }
 
 }
